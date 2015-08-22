70956a84ffdfe3c0cb6048589b5fa5edc7a3928b
Rishub Jain <rishubjain@gmail.com>
2015-08-22 10:25:30 -0400

    Fixed comments

--- sbatch_simple.sh
+++ sbatch_simple.sh
@@ -5,28 +5,42 @@
 # 
 # READ THIS BEFORE USING THIS SCRIPT:
 # 
-# This script is designed to work with relion 1.3 and 1.4 on biowulf2 (and with a similar naming convention as the RELION 1.3 tutorial).
+# This script is designed to work with relion 1.3 and 1.4 on biowulf2. It is meant to run as if you have extracted particles using RELION, but if you didn't you can change things to make it still work.
+# This script just changes any star filenames from /lscratch/JOBID/ to Particles/Micrographs/, and sets all the biowulf2 parameters.
 #
-# Example sbatch submit command using the largemem partition and 15.9G memory per task (inputing the other parameters in the RELION GUI):
-# 
-# sbatch --mem=1009g --partition=largemem
+# GUI Information:
+# Number of MPI procs: This is the number of different processes (tasks) you want to run
+# Number of threads: This is the number of CPUs allocated to each task. Change this from the default of 1 if you need more memory per task.
+# Available RAM (in Gb) per thread: This is the amount of memory allocated per CPU. You can find this out by dividing the memory per node by the number of CPUs available
+#
+# Example sbatch submit command within RELION using the largemem partition and 32G memory per task, with 32 tasks:
+# Number of MPI procs: 32
+# Number of threads: 2
+# Available RAM (in Gb) per thread: 15.9 
+# Submit to queue?: Yes
+# Queue name: <LEAVE BLANK>
+# Queue submit command: sbatch --mem=1009g --partition=largemem
+# Standard submission script: sbatch_new_lscratch_rel14.sh (or wherever it is located)
+#
 #
 # Things to note:
-# - m is the name of the Micrographs folder. This is by default "Micrographs", so if your Micrographs folder is called something else, you have to specify it by putting --export=m=foldername in the sbatch submit command in the RELION GUI
+# - You should have a good idea about how clusters, specifically biowulf2, work when using this script (please read the biowulf2 user guide).
+# - This script will work for RELION 1.3 and 1.4 because the actual naming conventions do not change between these versions. It probably also works with RELION 1.2, but I am not sure.
+# - The script assumes that the particle stack(s) are in the Particles/Micrographs/ directory, which should be in the home directory (the directory the GUI was run from). If this is not the case, an easy fix is to create a Particles/Micrographs/ directory in the home directory, and link the Particle stack from wherever it is to the Particles/Micrographs/ directory. You will also have to change this in the input star file (or in the data.star if you are continuing a run) to also include this. The particles in the star file must be in the format of Particles/<folder name>/<particle>.mrcs OR /lscratch/<JOB ID>/<particle>.mrcs.
+# - To link files, type the following into the command line:
+#		ln -s /complete/path/to/file.mrcs /path/to/home/directory/
+# - m is the name of the Micrographs folder (which is also the Particles/<folder name>/ folder, which will have the same folder name if extracted through RELION). This is by default "Micrographs", so if your Micrographs folder is called something else, you have to specify it by putting --export=m=foldername in the sbatch submit command in the RELION GUI
+# - To export multiple variables, use --export=ALL,m=Micrographs,maxs=830000000 in the submit command in the RELION GUI. You must have the =ALL at the beginning.
+# - If you do not need 10 days to run, you should specify a more reasonable time using --time=4-00:00:00.
+# - This script is specific to how biowulf2 functions as of August 2015 (how it specifies the list of nodes, etc.). As biowulf2 changes this script should be changed.
 # - By default biowulf2 uses hyperthreading, and it seems like if you specify --ntasks-per-core=2, things get messed up (as of July 30 2015). Though I haven't tried this, specifying this option may solve wierd problems you might encounter.
-# - The main input particles star file should be in the home directory (the directory the GUI was run from). Otherwise you might encounter some problems. You can also link this starfile. To link files, type the following into the command line:
-#       ln -s /complete/path/to/file.star /path/to/home/directory/
-# - You cannot have dashes (or any other special characters) in the names of the Particle stack .mrcs files nor in the main particle star file
-# - If you do not need 10 days to run, please specify a more reasonable time (less or more, helps you and other users).
-# - The star files are changed to have /lscratch/JOBID/ instead of Particles/Micrographs/ as the folder that holds the .mrcs files. The original star file is copied to *.star.save.
-# - I initially stored this /lscratch/JOBID/ information in a new star file and edited the command to use that star file, but to do this sucessfully you would have to change the actual rootname of the files (like run1, autopicking, etc.). This can be conflicting in many places, since relion assumes some filesnames to be a certain way. To avoid this hedache, I am just editing over the same star file.
 # - The *_data.star files with the particle information, and any other particle star files that were created, will have the particles listed as being in the lscratch directory, so when viewing the particles you will have to change this. You do not need to change this if you are just going from step to step. This script still works if the star files have /lscratch/JOBID/particle.mrcs instead of Particle/Micrographs/particle.mrcs
 # - If you want to view the particles manually, you can type this into the command line:
 #       cat particles.star | perl -pe "s|/lscratch/.*?/|Particles/Micrographs/|" > particles.star.mod
 #       mv particles.star.mod particles.star
 # - This script is specific to how biowulf2 functions as of August 2015 (how it specifies the list of nodes, etc.). As biowulf2 changes this script should be changed.
 # - Allocate the memory by node (using --mem-125g) instead of by cpu (--mem-per-cpu=4g).
-
+#
 
 # Setting up things
 
